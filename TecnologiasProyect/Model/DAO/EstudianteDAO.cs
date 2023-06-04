e9d024815d701eef93b288f08631ef3690bd50ef
DracoGilga <cesar96707@gmail.com>
2023-06-04 00:21:46 -0600

    Creacion de la carpeta DAO y clases DAO modificacion de EstudianteDAO

---
+++ TecnologiasProyect/Model/DAO/EstudianteDAO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TecnologiasProyect.Model.DAO
+{
+    public class EstudianteDAO
+    {
+        public static Boolean GuardarEstudiante(Estudiante estudianteNuevo)
+        {
+            try
+            {
+                DataClassTutoriaDataContext conexionBD = GetConexion();
+                var estudiante = new Estudiante()
+                {
+                    matricula = estudianteNuevo.matricula,
+                    nombre = estudianteNuevo.nombre,
+                    apellidoPaterno = estudianteNuevo.apellidoPaterno,
+                    apellidoMaterno = estudianteNuevo.apellidoMaterno,
+                    correoPersonal = estudianteNuevo.correoPersonal,
+                    correoInstitucional = estudianteNuevo.correoInstitucional,
+                    IdTutor = estudianteNuevo.IdTutor,
+                    IdProgramaEducativo = estudianteNuevo.IdProgramaEducativo
+                };
+                conexionBD.Estudiante.InsertOnSubmit(estudiante);
+                conexionBD.SubmitChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public static DataClassTutoriaDataContext GetConexion()
+        {
+            return new DataClassTutoriaDataContext(global::System.Configuration.
+                ConfigurationManager.ConnectionStrings["TecnologiasConnectionString"].ConnectionString);
+        }
+    }
+}
