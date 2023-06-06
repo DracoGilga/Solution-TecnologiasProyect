fd492a471b4508c62818a0fdb747e230bbc3db3f
Gloria-Mendoza <gm633980@gmail.com>
2023-06-06 00:09:58 -0600

    Agregacion de AcademicoDAO

--- TecnologiasProyect/Model/DAO/AcademicoDAO.cs
+++ TecnologiasProyect/Model/DAO/AcademicoDAO.cs
@@ -7,5 +7,34 @@ namespace TecnologiasProyect.Model.DAO
 {
     public class AcademicoDAO
     {
+        public static Boolean RegistrarTutorAcademico(Academico academicoNuevo)
+        {
+            try
+            {
+                DataClassesTutoriaDataContext conexionBD = GetConexion();
+                var tutor = new Academico()
+                {
+                    noPersonal = academicoNuevo.noPersonal,
+                    nombre = academicoNuevo.nombre,
+                    apellidoPaterno = academicoNuevo.apellidoPaterno,
+                    apellidoMaterno = academicoNuevo.apellidoMaterno,
+                    correoPersonal = academicoNuevo.correoPersonal,
+                    correoInstitucional = academicoNuevo.correoInstitucional,
+                    password = academicoNuevo.password
+                };
+                conexionBD.Academico.InsertOnSubmit(tutor);
+                conexionBD.SubmitChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public static DataClassesTutoriaDataContext GetConexion()
+        {
+            return new DataClassesTutoriaDataContext(global::System.Configuration.
+                ConfigurationManager.ConnectionStrings["TecnologiasConnectionString"].ConnectionString);
+        }
     }
 }
