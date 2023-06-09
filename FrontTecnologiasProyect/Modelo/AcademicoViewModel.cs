bb520692af0d3f405acbeb2c397d39f83e417247
Gloria-Mendoza <gm633980@gmail.com>
2023-06-09 14:07:11 -0600

    Creacion de AcademicoViewModel

---
+++ FrontTecnologiasProyect/Modelo/AcademicoViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontTecnologiasProyect.Modelo
+{
+    class AcademicoViewModel
+    {
+        public ObservableCollection<Academico> academicoBD { get; set; }
+
+        public AcademicoViewModel(Academico academico)
+        {
+            academicoBD = new ObservableCollection<Academico>();
+            GuardarTutorAcademico(academico);
+        }
+        public async void GuardarTutorAcademico(Academico academico)
+        {
+            var conexionServicios = new Service1Client();
+            if (conexionServicios != null)
+            {
+                Boolean resultado = await conexionServicios.RegistrarAcademicoAsync(academico);
+            }
+        }
+    }
+}
