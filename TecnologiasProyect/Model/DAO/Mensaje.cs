3c2ca6c018d612adb3ec9efa37da314137344750
Crisologo11 <crisologo.david@hotmail.com>
2023-06-09 00:56:27 -0600

    mensajes update

---
+++ TecnologiasProyect/Model/DAO/Mensaje.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TecnologiasProyect.Model
+{
+    public class Mensaje
+    {
+        public Boolean error { get; set; }
+        public string mensaje { get; set; }
+        public Academico usuarioAutenticado { get; set; }
+    }
+}
