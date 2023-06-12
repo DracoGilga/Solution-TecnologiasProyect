6ffe4f4d70ce4a986b0a01608a3162b7b3037cd0
DracoGilga <cesar96707@gmail.com>
2023-06-11 18:49:54 -0600

    RegistrarAlumno ya guarda.

--- TecnologiasProyect/IService1.cs
+++ TecnologiasProyect/IService1.cs
@@ -52,26 +52,6 @@ namespace TecnologiasProyect
         List<ProgramaEducativo> ObtenerProgramas();
         [OperationContract]
         List<TipoProblematica> ObtenerTipoProblematicas();
-    }
-
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
+        
     }
 }
