66ffb6cd15b4508264e3c1797f12d5ac8e3be88f
DracoGilga <cesar96707@gmail.com>
2023-06-15 01:44:18 -0600

    Creacion de ventana Modificar Alumno

---
+++ FrontTecnologiasProyect/ModificarAlumno.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FrontTecnologiasProyect
+{
+    /// <summary>
+    /// Lógica de interacción para ModificarAlumno.xaml
+    /// </summary>
+    public partial class ModificarAlumno : Window
+    {
+        public ModificarAlumno()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_AgregarTabla(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_ModificarEstudiante(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void CargarCombobox()
+        {
+
+        }
+    }
+}
